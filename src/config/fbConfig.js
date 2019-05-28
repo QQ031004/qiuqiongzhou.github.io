285a4a7f6f462437917011c1c6fc798d1ab68583
QQ031004 <47665716+QQ031004@users.noreply.github.com>
2019-05-28 10:51:04 +0200

    Har tagit bort APIkey från GitHub 2019-05-28)

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -6,12 +6,7 @@ import 'firebase/auth'
 // Initialize Firebase
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyD7g52zjOuE-1Fb3UsMwn8-7YcnqejtqrA",
-    authDomain: "qq-testapp-97a98.firebaseapp.com",
-    databaseURL: "https://qq-testapp-97a98.firebaseio.com",
-    projectId: "qq-testapp-97a98",
-    storageBucket: "qq-testapp-97a98.appspot.com",
-    messagingSenderId: "288771212150"
+    (Har tagit bort APIkey från GitHub 2019-05-28)
   };
 
 
